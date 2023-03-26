c35d06a1832f3e758b2c6dedfa2870c9118726b6
dawitAsma <david@habeshnet.com>
2023-03-26 14:42:58 +0100

    init

--- personal-home-page-template.markdown
+++
@@ -1,7 +0,0 @@
-Personal Home Page  Template
-----------------------------
-
-
-A [Pen](https://codepen.io/Dawit0101/pen/vYzvNrM) by [Dawit Asmerawork](https://codepen.io/Dawit0101) on [CodePen](https://codepen.io).
-
-[License](https://codepen.io/license/pen/vYzvNrM).
--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 */
 
 const {
-  core: { test, expect, run },
-  prettify
+    core: { test, expect, run },
+    prettify
 } = window.jestLite;
 
 /* =================
@@ -16,15 +16,15 @@ const {
    ===================
 */
 const getHeader = document.querySelectorAll("header"),
-  getH1 = document.querySelectorAll("h1"),
-  getSiteHeader = document.querySelectorAll(".c-site-header"),
-  getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
-  getMain = document.querySelectorAll("main"),
-  getFooter = document.querySelectorAll("footer"),
-  getSiteFooter = document.querySelectorAll(".c-site-footer"),
-  getIFrame = document.querySelectorAll("iframe"),
-  getImage = document.querySelectorAll("img"),
-  getWords = document.body.innerText;
+    getH1 = document.querySelectorAll("h1"),
+    getSiteHeader = document.querySelectorAll(".c-site-header"),
+    getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
+    getMain = document.querySelectorAll("main"),
+    getFooter = document.querySelectorAll("footer"),
+    getSiteFooter = document.querySelectorAll(".c-site-footer"),
+    getIFrame = document.querySelectorAll("iframe"),
+    getImage = document.querySelectorAll("img"),
+    getWords = document.body.innerText;
 
 /* =================
    ASSERTIONS 
@@ -34,34 +34,34 @@ const getHeader = document.querySelectorAll("header"),
    ===================
 */
 test("There is at least one header element", () => {
-  expect(getHeader.length).toBeGreaterThanOrEqual(1);
+    expect(getHeader.length).toBeGreaterThanOrEqual(1);
 });
 test("There is at least one h1", () => {
-  expect(getH1.length).toBeGreaterThanOrEqual(1);
+    expect(getH1.length).toBeGreaterThanOrEqual(1);
 });
 test("There is only one header element with the class c-site-header", () => {
-  expect(getSiteHeader.length).toBe(1);
+    expect(getSiteHeader.length).toBe(1);
 });
 test("There is a nav element with an aria-label of Main Site Links.", () => {
-  expect(getAria.length).toBeGreaterThanOrEqual(1);
+    expect(getAria.length).toBeGreaterThanOrEqual(1);
 });
 test("There is only one main element", () => {
-  expect(getMain.length).toBe(1);
+    expect(getMain.length).toBe(1);
 });
 test("There is at least one footer element", () => {
-  expect(getFooter.length).toBeGreaterThanOrEqual(1);
+    expect(getFooter.length).toBeGreaterThanOrEqual(1);
 });
 test("There is only one footer element with the class c-site-footer", () => {
-  expect(getSiteFooter.length).toBe(1);
+    expect(getSiteFooter.length).toBe(1);
 });
 test("There is embedded video", () => {
-  expect(getIFrame.length).toBeGreaterThanOrEqual(1);
+    expect(getIFrame.length).toBeGreaterThanOrEqual(1);
 });
 test("There is at least one image", () => {
-  expect(getImage.length).toBeGreaterThanOrEqual(1);
+    expect(getImage.length).toBeGreaterThanOrEqual(1);
 });
 test("There are at least 500 words on the page", () => {
-  expect(getWords.length).toBeGreaterThanOrEqual(500);
+    expect(getWords.length).toBeGreaterThanOrEqual(500);
 });
 
 const console = document.getElementById("tests");
